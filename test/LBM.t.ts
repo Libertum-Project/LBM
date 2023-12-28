19597c77110a80d69997d2cd68b8cb815651629f
Christopher Tham <chriswt91@gmail.com>
2023-12-28 01:28:51 -0800

    Add core team vesting tests

--- test/LBM.t.ts
+++ test/LBM.t.ts
@@ -46,7 +46,10 @@ describe("LBM", function () {
       const { LBM } = await loadFixture(deployAll);
       expect(await LBM.name()).to.equal("Libertum");
       expect(await LBM.symbol()).to.equal("LBM");
-      console.log("TOTAL LBM SUPPLY", ethers.formatEther(await LBM.totalSupply()));
+      console.log(
+        "TOTAL LBM SUPPLY",
+        ethers.formatEther(await LBM.totalSupply())
+      );
     });
   });
 
@@ -59,43 +62,43 @@ describe("LBM", function () {
     });
   });
 
-  describe("Vesting", function () {
-    it("Should vest core team tokens correctly", async function () {
-      const {
-        admin,
-        treasuryWallet,
-        presaleWallet,
-        cexWallet,
-        liquidityWallet,
-        stakingWallet,
-        projectDevelopmentWallet,
-        CoreTeam,
-        LBM,
-      } = await loadFixture(deployAll);
+  describe("Core Team Vesting Contract", function () {
+    it("Should not allow vesting before the 12 month cliff", async function () {
+      const { CoreTeam, LBM } = await loadFixture(deployAll);
       expect(await CoreTeam["releasable(address)"](LBM.target)).to.equal(0);
+    });
 
-      await time.increase(time.duration.days(30) * 11);
-      expect(await CoreTeam["releasable(address)"](LBM.target)).to.equal(0);
+    it("Should allow vesting after the 12 month cliff, linearly over 48 months", async function () {
+      const { admin, CoreTeam, LBM } = await loadFixture(deployAll);
 
-      await time.increase(time.duration.days(60));
-      console.log(
-        "Core team vesting:",
-        ethers.formatEther(await CoreTeam["releasable(address)"](LBM.target))
+      // 13 months into vesting period
+      await time.increase(time.duration.days(30) * 13);
+
+      // Expected release amount: 50,000,000 tokens / 48 months ~= 1,041 tokens
+      const expectedReleaseAmount = ethers.getBigInt(
+        "1041666666666666666666666"
+      );
+      // Acceptable delta (approximately 5 minutes) ~= 120 tokens
+      const delta = ethers.getBigInt("120563271604938271600");
+      const actualReleaseAmount = await CoreTeam["releasable(address)"](
+        LBM.target
       );
 
+      expect(actualReleaseAmount).to.be.closeTo(expectedReleaseAmount, delta);
       await CoreTeam["release(address)"](LBM.target);
-      console.log(
-        "Core team vesting:",
-        ethers.formatEther(await CoreTeam["releasable(address)"](LBM.target))
-      );
-      console.log(
-        "Contract balance:",
-        ethers.formatEther(await LBM.balanceOf(CoreTeam.target))
-      );
-      console.log(
-        "treasuryWallet, presaleWallet, cexWallet, liquidityWallet, stakingWallet, projectDevelopmentWallet balance:",
-        ethers.formatEther(await LBM.balanceOf(admin.address))
-      );
+
+      const adminBalance = await LBM.balanceOf(admin.address);
+      expect(adminBalance).to.be.closeTo(expectedReleaseAmount, delta);
+
+      // At 47 months
+      await time.increase(time.duration.days(30) * 46);
+      await CoreTeam["release(address)"](LBM.target);
+      expect(await LBM.balanceOf(CoreTeam.target)).to.not.equal(0);
+
+      // At 48 months
+      await time.increase(time.duration.days(30) * 1);
+      await CoreTeam["release(address)"](LBM.target);
+      expect(await LBM.balanceOf(CoreTeam.target)).to.equal(0);
     });
   });
 });
